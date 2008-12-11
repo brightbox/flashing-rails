af76fe9911f447cdbe47a34de172e866ef93ecc0
Caius Durling <git@caius.name>
2008-12-11 22:25:59 +0000

    Tidying up the HTML output.

--- lib/flashing_rails.rb
+++ lib/flashing_rails.rb
@@ -1,7 +1,7 @@
 module Brightbox
   module FlashingRails
     module ViewHelpers
-      
+
       # Outputs all flash messages as an unordered list
       # Returns the HTML as a string or true if flash is empty
       def flash_messages( opts = {} )
@@ -10,13 +10,12 @@ module Brightbox
         css_id = opts[:id] || "flash"
         css_class = opts[:class] || nil
         
-        msgs = []
-        flash.each do |key, message|
-          msgs << "\t" + content_tag("li", message, :class => key)
+        msgs = flash.map do |key, message|
+          "\t" + content_tag("li", message, :class => key)
         end.unshift("") << "" # Force a newline before/after items
         content_tag("ul", msgs.join("\n"), :id => css_id)
       end
-            
+
     end
   end
 end
