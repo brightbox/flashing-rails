53eda9be61c9c618ad2cef6fd90e445bbeedc099
Caius Durling <caius@brightbox.co.uk>
2010-03-19 14:43:26 +0000

    Adding the ability to pass arrays of messages to flash[].
    flash[:notice] = ["foo", "bar"]

--- lib/flashing_rails.rb
+++ lib/flashing_rails.rb
@@ -9,10 +9,19 @@ module Brightbox
         
         css_id = opts[:id] || "flash"
         css_class = opts[:class] || nil
-        
+
+        generate_flash_list = lambda do |key, message|
+          if message.is_a?(Array)
+            message.map {|msg| generate_flash_list[key, msg] }.join("\n")
+          else
+            "\t" + content_tag("li", message, :class => key)
+          end
+        end
+
         msgs = flash.map do |key, message|
-          "\t" + content_tag("li", message, :class => key)
+          generate_flash_list[key, message]
         end.unshift("") << "" # Force a newline before/after items
+
         content_tag("ul", msgs.join("\n"), :id => css_id)
       end
 
